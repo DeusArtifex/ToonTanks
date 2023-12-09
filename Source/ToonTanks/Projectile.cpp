96fd494fa86ffdc48641d936b7efd7a3a1d1b0fe
DeusArtifex <deus3artifex@gmail.com>
2023-12-10 00:39:15 +0100

    Change OnHit and DamageTaken behavior
    Change OnHit and DamageTaken behavior so they properly apply damage and destroy the projectile.

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Projectile.h"
+#include "Kismet/GameplayStatics.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
@@ -35,8 +36,16 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) 
 {
-	UE_LOG(LogTemp, Warning, TEXT("OnHit - %s"), *HitComp->GetName());
-	UE_LOG(LogTemp, Warning, TEXT("Hit - %s"), *OtherActor->GetName());
-	UE_LOG(LogTemp, Warning, TEXT("HitComp - %s"), *OtherComp->GetName());
+	auto MyOwner = GetOwner();
+	if (MyOwner == nullptr) return;
+
+	auto MyOwnerInstigator = MyOwner->GetInstigatorController();
+	auto DamageTypeClass = UDamageType::StaticClass();
+
+	if(OtherActor && OtherActor != this && OtherActor != MyOwner)
+	{
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
+		Destroy();
+	}
 }
 
--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -25,6 +25,9 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Movement")
 	class UProjectileMovementComponent* ProjectileMovement;
 
+	UPROPERTY(EditAnywhere)
+	float Damage = 50.f;
+
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
